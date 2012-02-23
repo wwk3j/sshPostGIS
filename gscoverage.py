5ce1fda5848e9bafc9325f2c2ea6e933af186069
Eric Rochester <err8n@virginia.edu>
2012-02-23 15:46:58 -0500

    Pull the bounding box from PostGIS.

--- gscoverage.py
+++ gscoverage.py
@@ -3,6 +3,7 @@
 from collections import namedtuple
 from contextlib import closing, contextmanager
 import pprint
+import re
 import time
 
 import psycopg2
@@ -26,6 +27,15 @@ ATTR_TYPES = {
         'POLYGON'      : 'com.vividsolutions.jts.geom.Polygon',
         'TRIANGLE'     : 'com.vividsolutions.jts.geom.Triangle',
         }
+reEXTENT = re.compile(r'''
+    ^ BOX \(
+        ( [\d\.-]* ) \s+ ( [\d\.-]* )
+        , \s*
+        ( [\d\.-]* ) \s+ ( [\d\.-]* )
+    \) $
+    ''',
+    re.VERBOSE,
+    )
 
 NATIVE_CRS = '''
 PROJCS["WGS 84 / UTM zone 18N", 
@@ -86,6 +96,7 @@ def pg_to_attribute(c, row):
     """
 
     (table_name, col_name, nillable, type_name) = row
+    is_geom = False
 
     attr_type = ATTR_TYPES.get(type_name)
     if attr_type is None:
@@ -93,38 +104,68 @@ def pg_to_attribute(c, row):
                     col_name, table_name))
         # Since there should only be one geomtry type, only take the first one.
         attr_type = ATTR_TYPES.get(c.fetchone()[0])
+        is_geom   = True
 
     return {
             'name'     : col_name,
             'nillable' : nillable,
             'binding'  : attr_type,
+            'is_geom'  : is_geom,
             }
 
 
-def load_attributes(db_params, table_name):
+def load_attributes(cxn, table_name):
     """This queries the Postgres table to get the attributes for creating the layer. """
-    with closing(psycopg2.connect(**db_params)) as cxn:
-        with closing(cxn.cursor()) as c:
-            c.execute('''
-                SELECT table_name, column_name, is_nullable, udt_name
-                FROM information_schema.columns
-                WHERE table_name=%s;
-                ''',
-                (table_name,)
-                )
-            col_infos = c.fetchall()
-            return [ pg_to_attribute(c, row) for row in col_infos ]
+    with closing(cxn.cursor()) as c:
+        c.execute('''
+            SELECT table_name, column_name, is_nullable, udt_name
+            FROM information_schema.columns
+            WHERE table_name=%s;
+            ''',
+            (table_name,)
+            )
+        col_infos = c.fetchall()
+        return [ pg_to_attribute(c, row) for row in col_infos ]
+
+
+def get_bounding_box(cxn, table_name, col_name):
+    """This queries the PostGIS table for the native bounding box. """
+    with closing(cxn.cursor()) as c:
+        c.execute('SELECT ST_Extent("{col}") FROM "{table}";'.format(
+            col=col_name, table=table_name,
+            ))
+        extent = c.fetchone()[0]
+
+        m = reEXTENT.match(extent)
+        if m is None:
+            log('WARNING: Invalid bounding box: %s' % (extent,))
+            bounding = BoundingBox(0, 0, 0, 0)
+        else:
+            bounding = BoundingBox._make(
+                    float(m.group(n)) for n in range(1, 5)
+                    )
+
+        return bounding
 
 
 def create_postgis_layer(
-        cat, wspace, dstore, name, srs, bounds, native_crs, db_params, log
+        cat, wspace, dstore, name, srs, native_crs, db_params, log
         ):
     """This creates and returns a new layer. """
     log('create_postgis_layer')
 
-    attributes = load_attributes(db_params, name)
-    native     = name
-    title      = name
+    with closing(psycopg2.connect(**db_params)) as cxn:
+        attributes = load_attributes(cxn, name)
+
+        for attr in attributes:
+            if attr['is_geom']:
+                bounds = get_bounding_box(cxn, name, attr['name'])
+                break
+        else:
+            bounds = BoundingBox(0, 0, 0, 0)
+
+    native = name
+    title  = name
 
     return cat.create_postgres_layer(
             wspace.name, dstore.name, name, native, title, srs, attributes,
@@ -148,10 +189,6 @@ def main():
     name_prefix = '%s%s' % (dbname, run)
     geo_type    = 'com.vividsolutions.jts.geom.MultiPolygon'
     srs         = 'EPSG:32618'
-    bounds      = BoundingBox(
-            -77.65885535032194, 18.49018108988885,
-            -77.63576126294949, 18.502941534123202,
-            )
     native_crs  = NATIVE_CRS
 
     # Now, for the actual processing.
@@ -160,7 +197,7 @@ def main():
                                   'uri://' + name_prefix)
     data_store = create_ds(cat, wspace, 'ds_' + name_prefix, dbname, db_params)
     layer      = create_postgis_layer(cat, wspace, data_store, layer_name, srs,
-                                      bounds, native_crs, db_params, log)
+                                      native_crs, db_params, log)
 
 
 if __name__ == '__main__':
